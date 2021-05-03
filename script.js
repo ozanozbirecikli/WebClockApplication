1043ffdc1b6195a123132f9c50bb7d584f58e6ae
Ozan Özbirecikli <41952823+ozanozbirecikli@users.noreply.github.com>
2021-05-04 00:13:01 +0300

    First Commit
    Firsts version of the chronometer implemented

---
+++ script.js
@@ -0,0 +1,73 @@
+var hour = 0;
+var minute = 0;
+var second = 0;
+var milisecond = 0;
+
+var hourText = "";
+var minuteText = "";
+var secondText = "";
+var milisecondText = "";
+
+var stopped = false;
+
+
+function chronometer(){
+    milisecond++;
+    if(milisecond === 100){
+        milisecond = 0;
+        second ++;
+        
+        if(second === 60){
+            second = 0;
+            minute++;
+
+            if(minute === 60){
+                minute = 0;
+                hour++;
+            }
+        }
+    }
+
+    milisecondText = updateText(milisecond);
+    secondText = updateText(second);
+    minuteText = updateText(minute);
+    hourText = updateText(hour);
+
+    document.getElementById("display").innerHTML = hourText + ":" + minuteText + ":" + secondText + ":" + milisecondText;
+
+}
+
+function updateText(value){
+    if(value < 10)
+        return "0" + value.toString();
+    
+    return value.toString();
+}
+
+function start(){
+    
+    if(stopped){
+        window.clearInterval(interval);
+        document.getElementById("start-button").innerHTML = "Start";
+        stopped = false;
+        document.getElementById("message").style.visibility = "visible"
+    }else{
+        interval = window.setInterval(chronometer, 10);
+        document.getElementById("start-button").innerHTML = "Pause";
+        stopped = true;
+        document.getElementById("message").style.visibility = "hidden"
+    }
+}
+
+function clearTime(){
+
+    window.clearInterval(interval);
+    milisecond = 0;
+    second = 0;
+    minute = 0;
+    hour = 0;
+    stopped = false;
+    document.getElementById("display").innerHTML = "00:00:00:00";
+    document.getElementById("start-button").innerHTML = "Start";
+    document.getElementById("message").style.visibility = "hidden"
+}
