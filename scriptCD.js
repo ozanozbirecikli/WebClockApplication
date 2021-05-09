ef297629b79298babb0b24449c5d53922d832289
Onur Eker <60039027+onureker07@users.noreply.github.com>
2021-05-09 16:20:30 +0300

    Webstorage is added
    Webstorage is added

--- scriptCD.js
+++ scriptCD.js
@@ -1,5 +1,7 @@
-var globalID = 0
-var intervals = []
+var globalID = localStorage.getItem("globalID") ? localStorage.getItem("globalID"): 0
+var intervals = {}
+window.onload = reloadPage
+
 class countDown{
     constructor(name,hour,min,sec,id){
         this.name = "Title: " + name
@@ -16,6 +18,8 @@ class countDown{
 
 function update(cdObject){
     toWrite = document.getElementById(cdObject.id)
+
+    localStorage.setItem(cdObject.id, cdObject.name.substring(7) + ":" + cdObject.hour + ":" + cdObject.min + ":" +cdObject.sec)
 
     if (cdObject.sec==0){
         if(cdObject.min==0){
@@ -42,37 +46,45 @@ function update(cdObject){
 function deleteClick(id){
     noofID = id.id.toString()
     noofID = parseInt(noofID.substring(3),10)
-    console.log(noofID)
+
+    cdss = localStorage.getItem("cds")
+    
+    cdss = cdss.replace(noofID+"-id:","")
+    cdss = cdss.replace(noofID+"-id","")
+    localStorage.setItem("cds",cdss)
+    
+
     temp = document.getElementById(id.id).parentNode
-    console.log(temp);
     temp.parentNode.removeChild(temp)
+
     window.clearInterval(intervals[noofID])
 }
 
-function addCountDown(){
+function addCountDown(name, hour, minute, second){
     division = document.getElementById("warning").innerHTML= ""
     var CDdiv = document.createElement("div")
     var pCountDown = document.createElement("pre")
     pCountDown.setAttribute("id", (globalID+"-id"))
 
-    var name = document.getElementById("name").value
-    document.getElementById("name").value = ""
-    
+    if (!name && !hour && !minute && !second){
+        var name = document.getElementById("name").value
+        document.getElementById("name").value = ""
+        
 
-    var hour = document.getElementById("hour").value
-    document.getElementById("hour").value = ""
+        var hour = document.getElementById("hour").value
+        document.getElementById("hour").value = ""
 
-    var minute = document.getElementById("minute").value
-    document.getElementById("minute").value = ""
+        var minute = document.getElementById("minute").value
+        document.getElementById("minute").value = ""
 
-    var second = document.getElementById("second").value
-    document.getElementById("second").value = ""
+        var second = document.getElementById("second").value
+        document.getElementById("second").value = ""
 
-    hour = parseInt(hour,10)
-    minute = parseInt(minute,10)
-    second = parseInt(second,10)
-
-    if (!isNaN(hour) && !isNaN(minute) && !isNaN(second) && (name!=="")){
+        hour = parseInt(hour,10)
+        minute = parseInt(minute,10)
+        second = parseInt(second,10)
+    }
+    if (!isNaN(hour) && !isNaN(minute) && !isNaN(second) && (name!=="") && minute<60 && second <60){
         const cdObject = new countDown(name,hour,minute,second,globalID)
         
         deleter = document.createElement("button")
@@ -83,15 +95,61 @@ function addCountDown(){
         CDdiv.appendChild(deleter)
 
         globalID++        
-
+        localStorage.setItem("globalID",globalID)
         CDdiv.appendChild(pCountDown)
         CDdiv.classList.add("countDownStyle")
         document.body.appendChild(CDdiv)
 
+        cds = localStorage.getItem("cds")
+        if (cds)
+            localStorage.setItem("cds", cds + ":" + cdObject.getID())
+        else
+            localStorage.setItem("cds", cdObject.getID())
+        
+
         interval = window.setInterval(function(){update(cdObject)}, 1000);
-        intervals.push(interval)
+        intervals[globalID-1] = interval 
     }
     else{
         division = document.getElementById("warning").innerHTML= "One of the input is wrong, or more!"
     }
+}
+
+
+function reloadPage(){
+    ids = localStorage.getItem("cds")
+
+    if (ids.length >0)
+        ids = ids.split(":")
+    else return
+
+    if (ids.length > 0)
+        for (i = 0; i < ids.length; ++i){
+            existing = ids[i]
+            if (!existing||existing==="") continue
+            enumber = existing.substring(0,existing.length-3)
+
+            var CDdiv = document.createElement("div")
+            var pCountDown = document.createElement("pre")
+            pCountDown.setAttribute("id", (enumber+"-id"))
+            
+            content = localStorage.getItem(existing).split(":")
+            
+            const cdObject = new countDown(content[0],content[1],content[2],content[3],enumber)
+            
+            deleter = document.createElement("button")
+            deleter.setAttribute("id","div"+enumber)
+            deleter.innerHTML = "DELETE"
+            deleter.setAttribute("onclick","deleteClick(div"+enumber+")")
+            deleter.classList.add("deleteButton")
+            CDdiv.appendChild(deleter)
+        
+            CDdiv.appendChild(pCountDown)
+            CDdiv.classList.add("countDownStyle")
+            document.body.appendChild(CDdiv)
+
+            interval = window.setInterval(function(){update(cdObject)}, 1000);
+            intervals[enumber] = interval 
+        }
+
 }
