4a67e170d0dc80b5332a220bf816e5554be3fd11
ozanozbirecikli <ozan.ozbirecikli@turna.com>
2021-06-12 21:19:09 +0300

    footer updated

--- WebClockApplication-React/src/components/Footer.js
+++ WebClockApplication-React/src/components/Footer.js
@@ -7,20 +7,18 @@ const Footer = () => {
                 <div class="row">
                     
                     <div class="col-6 col-md">
-                        <h5>Features</h5>
+                        <h5>Versions</h5>
                         <ul class="list-unstyled text-small">
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
+                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/ozanozbirecikli/WebClockApplication/tree/main/WebClockApplication">Previous Version</a></li>
+                            <li class="mb-1"><a class="link-secondary text-decoration-none" >- Current Version 2.1</a></li>
+                            
                         </ul>
                     </div>
                     <div class="col-6 col-md">
                         <h5>Resources</h5>
                         <ul class="list-unstyled text-small">
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
-                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
+                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/ozanozbirecikli/WebClockApplication">GitHub</a></li>
+                            <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/ozanozbirecikli/WebClockApplication/blob/main/README.md">Readme</a></li>
                         </ul>
                     </div>
                     <div class="col-6 col-md">
